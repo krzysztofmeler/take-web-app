37fb4893941a34145c24659189bec20ee1f75856
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-06-22 21:05:06 +0200

    added styles to add new subject page

--- src/components/pages/AddNewSubjectPage.tsx
+++ src/components/pages/AddNewSubjectPage.tsx
@@ -1,10 +1,9 @@
-import { FC, useEffect, useState } from 'react';
-import { TextInput } from '../forms/TextInput';
-import { BasicSelector } from '../forms/BasicSelector';
+import { FC, useState } from 'react';
+import { Button, Card, Grid, Group, Text, TextInput } from '@mantine/core';
 import { jsSubmit } from '../../utils/js-submit';
-import { Lecturer } from '../../model/existing-objects/Lecturer';
 import { useRequest } from '../../hooks/useRequest.hook';
 import { settings } from '../../settings';
+import { update } from '../../utils/forms';
 
 const AddNewSubjectPage: FC = () => {
     const [name, setName] = useState<string>('');
@@ -22,19 +21,35 @@ const AddNewSubjectPage: FC = () => {
         });
     };
 
-    // useEffect(() => {
-    // }, [response]);
-
     return (
-        <form>
-            <TextInput value={name} updateValue={setName} label="Name" />
+        <Card withBorder shadow="md" maw={800} my={20} mx="auto">
+            <Group
+              gap={20}
+              p={10}
+              display="flex"
+              style={{ flexDirection: 'column', alignItems: 'start' }}
+            >
+                <Text component="h2" size="lg">
+                    Add new subject
+                </Text>
 
-            <input
-              onClick={jsSubmit(submit)}
-              type="submit"
-              value="Proceed and close"
-            />
-        </form>
+                <Grid maw={700}>
+                    <Grid.Col span={9}>
+                        <TextInput
+                          value={name}
+                          onChange={update(setName)}
+                          label="Name"
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={10}>
+                        <Button onClick={jsSubmit(submit)}>
+                            Proceed and close
+                        </Button>
+                    </Grid.Col>
+                </Grid>
+            </Group>
+        </Card>
     );
 };
 
