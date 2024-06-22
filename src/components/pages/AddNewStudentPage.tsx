ed1230a5bafc686e0708fe33c764f09ea9027f96
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-06-22 20:45:07 +0200

    added styles to add new student page

--- src/components/pages/AddNewStudentPage.tsx
+++ src/components/pages/AddNewStudentPage.tsx
@@ -1,9 +1,18 @@
 import { FC, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { TextInput } from '../forms/TextInput';
+import {
+    Button,
+    Card,
+    Grid,
+    Group,
+    MultiSelect,
+    Text,
+    TextInput,
+} from '@mantine/core';
 import { jsSubmit } from '../../utils/js-submit';
 import { useRequest } from '../../hooks/useRequest.hook';
 import { settings } from '../../settings';
+import { update } from '../../utils/forms';
 
 const AddNewStudentPage: FC = () => {
     const [firstName, setFirstName] = useState<string>('');
@@ -40,25 +49,44 @@ const AddNewStudentPage: FC = () => {
     }, [response]);
 
     return (
-        <form>
-            <TextInput
-              value={firstName}
-              updateValue={setFirstName}
-              label="First name"
-            />
-            <TextInput
-              value={lastName}
-              updateValue={setLastName}
-              label="Last name"
-            />
-            <TextInput value={email} updateValue={setEmail} label="E-mail" />
+        <Card withBorder shadow="md" maw={800} my={20} mx="auto">
+            <Group gap={20} p={10}>
+                <Text component="h2" size="lg">
+                    Add new student
+                </Text>
 
-            <input
-              onClick={jsSubmit(submit)}
-              type="submit"
-              value="Proceed and close"
-            />
-        </form>
+                <Grid maw={700}>
+                    <Grid.Col span={6}>
+                        <TextInput
+                          value={firstName}
+                          onChange={update(setFirstName)}
+                          label="Name"
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={6}>
+                        <TextInput
+                          value={lastName}
+                          onChange={update(setLastName)}
+                          label="Surname"
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={8}>
+                        <TextInput
+                          value={email}
+                          onChange={update(setEmail)}
+                          label="E-mail"
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={10}>
+                        <Button onClick={jsSubmit(submit)}>
+                            Proceed and close
+                        </Button>
+                    </Grid.Col>
+                </Grid>
+            </Group>
+        </Card>
     );
 };
 
