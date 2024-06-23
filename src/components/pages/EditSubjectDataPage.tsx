27825ab289cfd9ab7c82f01cc4a0f86916e44768
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-06-23 17:40:00 +0200

    added edit subject hook and edit subject page updated

--- src/components/pages/EditSubjectDataPage.tsx
+++ src/components/pages/EditSubjectDataPage.tsx
@@ -1,44 +1,77 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Card, Group, Text } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { useAsyncEffect } from '../../hooks/useAsyncEffect.hook';
 import { SubjectForm } from '../SubjectForm';
-import { request } from '../../utils/request';
 import { Subject } from '../../model/existing-objects/Subject';
+import { useGetSubjects } from '../../hooks/useGetSubjects.hook';
+import { useEditSubject } from '../../hooks/useEditSubject.hook';
+import { BasicRequestResult } from '../../types/BasicRequestResult';
+import { showNotification } from '../../utils/Notifications';
+import { sleep } from '../../utils/sleep';
+import { SubpageError } from '../SubpageError';
 
 const EditSubjectDataPage: FC = () => {
   const [name, setName] = useState<string>('');
 
   const { id } = useParams();
 
-  useAsyncEffect(async () => {
-    const response = await request.get('/subjects');
+  const { subjects, error: getSubjectsError } = useGetSubjects();
 
-    if (response.status === 200) {
-      const subject = (response.data as Subject[]).find((s) => s.id.toString() === id);
+  const { proceed: editSubject, result: editSubjectResult } = useEditSubject();
 
-      if (subject === undefined) {
-        // todo: handle as error
-      } else {
-        setName(subject.name);
-      }
-    }
-  }, []);
+  const [subject, setSubject] = useState<Subject | null>(null);
+
+  const [notFoundError, setNotFoundError] = useState(false);
 
   const navigate = useNavigate();
 
-  const submit = async () => {
-    const response = await request.put(`/subjects/${id}`, {
-      name,
-    });
+  useEffect(() => {
+    if (subjects !== null) {
+      const matchingSubject = subjects.find((s) => s.id.toString() === id);
 
-    if (response.status === 200) {
-      setTimeout(() => {
-        navigate(`/administration/subject-data/${id}`);
-      }, 500);
+      if (matchingSubject) {
+        setSubject(matchingSubject);
+        setName(matchingSubject.name);
+      } else {
+        setNotFoundError(true);
+      }
     }
+  }, [subjects]);
+
+  const submit = async () => {
+    await editSubject(id!, { name });
   };
+
+  useEffect(() => {
+    if (editSubjectResult === BasicRequestResult.Error) {
+      showNotification({
+        color: 'red',
+        title: 'An error occurred',
+        message: 'Unknown error occurred, check your data and try again or contact administrator.',
+      });
+    } else if (editSubjectResult === BasicRequestResult.Ok) {
+      showNotification({
+        color: 'green',
+        title: 'Success',
+        message: 'Subject data successfully updated',
+      });
+    }
+  }, [editSubjectResult]);
+
+  useAsyncEffect(async () => {
+    if (editSubjectResult === BasicRequestResult.Ok) {
+      await sleep(500);
+      navigate(`/administration/subject-data/${id}`);
+    }
+  }, [editSubjectResult]);
+
+  if (notFoundError) {
+    return <SubpageError text="Subject not found" />;
+  } else if (getSubjectsError) {
+    return <SubpageError text="Unknown error occurred" />;
+  }
 
   return (
     <Card withBorder shadow="md" maw={800} my={20} mx="auto">
@@ -48,7 +81,13 @@ const EditSubjectDataPage: FC = () => {
         </Text>
 
         <Group maw={700}>
-          <SubjectForm name={name} setName={setName} submit={submit} disableSubmit={false} loading={false} />
+          <SubjectForm
+            name={name}
+            setName={setName}
+            submit={submit}
+            disableSubmit={[BasicRequestResult.Loading, BasicRequestResult.Ok].includes(editSubjectResult)}
+            loading={editSubjectResult === BasicRequestResult.Loading}
+          />
         </Group>
       </Group>
     </Card>
